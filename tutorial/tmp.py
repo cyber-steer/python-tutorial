3a3eb7d145a6cb2c484fc80d386e0778887060b9
cyber-steer <cybersteer@naver.com>
2023-02-13 08:25:52 +0900

    update

--- tutorial/tmp.py
+++ tutorial/tmp.py
@@ -1,5 +1,7 @@
-a=(1,2,3)
-# a[0] = 5
-print(a)
-
-print(dir(a))
+struct ='''if 조건식:
+    print('조건입 참입니다')
+else:
+    print('조건입 거짓입니다')'''
+print(struct.find('조건식'))
+struct=struct.replace('조건식','True')
+print(struct)
